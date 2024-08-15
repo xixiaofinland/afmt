93a3a4f25057dc2de854d7df7cab55d462707c20
xi xiao <tdxiaoxi2@gmail.com>
2024-08-15 20:10:36 +0300

    chore: rearrange file structure

--- build.rs
+++ build.rs
@@ -1,13 +1,13 @@
 fn main() {
-    let src_dir = std::path::Path::new("src");
+    let dep_dir = std::path::Path::new("dep");
 
     let mut c_config = cc::Build::new();
-    c_config.std("c11").include(src_dir);
+    c_config.std("c11").include(dep_dir);
 
     #[cfg(target_env = "msvc")]
     c_config.flag("-utf-8");
 
-    let parser_path = src_dir.join("parser.c");
+    let parser_path = dep_dir.join("parser.c");
     c_config.file(&parser_path);
     println!("cargo:rerun-if-changed={}", parser_path.to_str().unwrap());
 
