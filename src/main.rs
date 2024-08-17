25e7ff883ef2b2370107d18741386b1bc70a207f
xi xiao <tdxiaoxi2@gmail.com>
2024-08-17 08:49:14 +0300

    basic version

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use afmt;
-use tree_sitter::{Language, Parser}; // Replace with the correct module path
+use tree_sitter::{Node, Parser}; // Replace with the correct module path
 
 fn main() {
     let mut parser = Parser::new();
@@ -7,14 +7,37 @@ fn main() {
         .set_language(&afmt::language())
         .expect("Error loading Apex grammar");
 
-    let code = r#"
-    public class MyClass {
-        public void myMethod() {
-            System.debug('Hello, world!');
+    let code = "public class MyClass {
+        public void hello() {
+        true;
+        }
+    }";
+
+    let mut tree = parser.parse(code, None).unwrap();
+    let root_node = tree.root_node();
+    if root_node.has_error() {
+        println!("root node found error!");
+        return;
+    }
+
+    traverse(root_node);
+}
+
+fn traverse(node: Node) {
+    if node.child_count() == 0 {
+        println!("Leaf node: {}", node.kind());
+        println!("Leaf node: {}", node.start_position());
+        if node.has_error() {
+            println!("error ndoe: {}", node.kind());
+        }
+    } else {
+        for i in 0..node.child_count() {
+            let child = node.child(i).unwrap();
+            println!("child node: {}", node.kind());
+            if node.has_error() {
+                println!("error ndoe: {} ", node.kind());
+            }
+            traverse(child);
         }
     }
-    "#;
-
-    let tree = parser.parse(code, None).unwrap();
-    println!("{:?}", tree.root_node().to_sexp());
 }
