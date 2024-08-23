7e7d79e5ae6c00946f9e7d83fd03c33a79b102d0
xi xiao <tdxiaoxi2@gmail.com>
2024-08-23 21:40:52 +0300

    class rewrite in progress

--- src/node_struct.rs
+++ src/node_struct.rs
@@ -57,18 +57,34 @@ impl<'a, 'tree> Class<'a, 'tree> {
 //https://github.com/dangmai/prettier-plugin-apex/blob/60db6549a441911a0ef25b0ecc5e61727dc92fbb/packages/prettier-plugin-apex/src/printer.ts#L612
 impl<'a, 'tree> Rewrite for Class<'a, 'tree> {
     fn rewrite(&self, shape: &Shape, context: &Context) -> Option<String> {
+        let mut result = String::new();
+
         let modifier_nodes = self.get_modifiers();
-        let value = modifier_nodes[0]
+        let value = modifier_nodes[1]
             .utf8_text(context.source_code.as_bytes())
             .ok()?;
 
-        println!("value: {}", value);
-
-        let result = modifier_nodes
+        let modifiers_doc = modifier_nodes
             .iter()
-            .map(|n| n.to_sexp())
-            .collect::<Vec<String>>()
-            .join("\n");
+            .map(|n| match n.utf8_text(context.source_code.as_bytes()).ok() {
+                None => "",
+                Some(v) => v,
+            })
+            .collect::<Vec<&str>>()
+            .join(" ");
+
+        result.push_str(&modifiers_doc);
+
+        result.push_str(" ");
+
+        let name_node = self.as_ast_node().child_by_field_name("name")?;
+        let name_node_value = name_node.utf8_text(context.source_code.as_bytes()).ok()?;
+
+        result.push_str(name_node_value);
+        result.push_str(" {\n");
+        result.push_str("}");
+
+        println!("result: {}", result);
         Some(result)
     }
 }
