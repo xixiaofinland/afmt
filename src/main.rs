e4ba7e39a574779e3688aef4bb548aa41847a4e7
xi xiao <tdxiaoxi2@gmail.com>
2024-08-23 17:46:44 +0300

    +

--- src/main.rs
+++ src/main.rs
@@ -15,20 +15,20 @@ fn main() -> Result<()> {
         .expect("Error loading Apex grammar");
     let code = fs::read_to_string("test/1.cls").unwrap();
     let tree = parser.parse(&code, None).unwrap();
-    if tree.root_node().has_error() {
+    let root_node = tree.root_node();
+
+    if root_node.has_error() {
         bail!("parsing with error, bail out quickly.")
     }
 
-    let result = format_code(&tree)?;
+    let result = format_code(&root_node)?;
     println!("format result: {}", result);
     Ok(())
 }
 
-fn format_code(tree: &Tree) -> Result<String> {
-    let root_node = tree.root_node();
-    let mut visitor = Visitor::new(&root_node);
-    visitor.walk_from_root();
-    Ok(visitor.get_formatted())
+fn format_code(root_node: &Node) -> Result<String> {
+    let mut visitor = Visitor::default();
+    visitor.walk(&root_node)
 }
 
 fn add_node_text(node: Node, source_code: &str, formatted: &mut String) {
--- src/visitor.rs
+++ src/visitor.rs
@@ -1,89 +1,43 @@
 use crate::node_struct::{Class, NodeKind, Rewrite};
-use crate::utility::NodeUtilities;
 use anyhow::{anyhow, Result};
-use tree_sitter::{Node, Tree, TreeCursor};
+use tree_sitter::Node;
 
-pub struct Visitor<'a> {
-    pub formatted: String,
-    pub block_indent: String,
-    pub indent_level: usize,
-    pub context: Context,
-    pub root_node: &'a Node<'a>,
-}
+#[derive(Default)]
+pub struct Visitor {}
 
-pub struct Context {
-    config: String,
-    parent_indent: String,
-}
-
-impl Context {
-    pub fn new() -> Self {
-        Context {
-            config: String::new(),
-            parent_indent: String::new(),
-        }
-    }
-}
-
-impl<'a> Visitor<'a> {
-    pub fn new(root_node: &'a Node) -> Self {
-        Visitor {
-            formatted: String::new(),
-            block_indent: String::from(' '),
-            indent_level: 0,
-            context: Context::new(),
-            root_node,
-        }
-    }
-
+impl Visitor {
     //https://github.com/dangmai/prettier-plugin-apex/blob/60db6549a441911a0ef25b0ecc5e61727dc92fbb/packages/prettier-plugin-apex/src/printer.ts#L612
-    pub fn walk_from_root(&mut self) -> Result<()> {
-        let mut cursor = self.root_node.walk();
+    pub fn walk(&mut self, node: &Node) -> Result<String> {
+        let mut result = String::new();
 
-        if cursor.goto_first_child() {
-            loop {
-                let child = cursor.node();
+        let mut cursor = node.walk();
+        for child in node.children(&mut cursor) {
+            let kind = NodeKind::from_kind(child.kind());
 
-                let kind = NodeKind::from_kind(child.kind());
-
-                match kind {
-                    NodeKind::ClassDeclaration => {
-                        let c = Class::new(&child);
-                        self.visit_class(&c)?;
-                    }
-                    NodeKind::MethodDeclaration => {
-                        //self.visit_method_node(node);
-                    }
-                    NodeKind::IfStatement => {
-                        //self.visit_if_node(node);
-                    }
-                    NodeKind::ForLoop => {
-                        //self.visit_for_node(node);
-                    }
-                    NodeKind::Unknown => !unimplemented!(),
+            match kind {
+                NodeKind::ClassDeclaration => {
+                    let c = Class::new(&child);
+                    self.visit_class(&c)?;
                 }
-
-                if !cursor.goto_next_sibling() {
-                    break;
+                NodeKind::MethodDeclaration => {
+                    //self.visit_method_node(node);
                 }
+                NodeKind::IfStatement => {
+                    //self.visit_if_node(node);
+                }
+                NodeKind::ForLoop => {
+                    //self.visit_for_node(node);
+                }
+                NodeKind::Unknown => !unimplemented!(),
             }
         }
-        Ok(())
+        Ok(result)
     }
 
-    pub fn visit_class(&mut self, c: &Class) -> Result<()> {
+    pub fn visit_class(&mut self, c: &Class) -> Result<String> {
         let a = c
             .rewrite()
             .ok_or_else(|| anyhow!("Format Class node failed!"))?;
-        self.push_str(&a);
-        Ok(())
-    }
-
-    pub fn get_formatted(&mut self) -> String {
-        self.formatted.clone()
-    }
-
-    fn push_str(&mut self, s: &str) {
-        self.formatted.push_str(s);
+        Ok(a)
     }
 }
