1c4878883d96410310e53138e379f713024ea8aa
xi xiao <tdxiaoxi2@gmail.com>
2024-08-29 09:38:22 +0300

    +

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 pub mod args;
 mod config;
 mod context;
-mod extension;
 mod macros;
 mod node_struct;
 mod utility;
@@ -16,26 +15,4 @@ pub fn format(args: Args) -> Vec<Result<String>> {
     let source_files = vec![args.path];
     let session = Session::new(config, source_files);
     session.format()
-}
-
-/// The content of the [`node-types.json`][] file for this grammar.
-//pub const NODE_TYPES: &str = include_str!("node-types.json");
-
-// Uncomment these to include any queries that this grammar contains
-// pub const HIGHLIGHTS_QUERY: &str = include_str!("../../queries/highlights.scm");
-// pub const INJECTIONS_QUERY: &str = include_str!("../../queries/injections.scm");
-// pub const LOCALS_QUERY: &str = include_str!("../../queries/locals.scm");
-// pub const TAGS_QUERY: &str = include_str!("../../queries/tags.scm");
-
-#[cfg(test)]
-mod tests {
-    use crate::config::language;
-
-    #[test]
-    fn test_can_load_grammar() {
-        let mut parser = tree_sitter::Parser::new();
-        parser
-            .set_language(language())
-            .expect("Error loading Apex grammar");
-    }
 }
