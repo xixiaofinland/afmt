0263412830ab1578457c26dde863f6ca151f7539
xi xiao <tdxiaoxi2@gmail.com>
2024-10-11 17:12:07 +0300

    +

--- benches/afmt_benchmark.rs
+++ benches/afmt_benchmark.rs
@@ -2,12 +2,10 @@ use afmt::{config::Session, format};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    // Create a session with a sample configuration and path
     let sample_path = "../large_apex_files/files/MetadataService2.cls";
     let session = Session::create_session_from_config(None, vec![sample_path.to_string()])
         .expect("Failed to create session");
 
-    // Benchmark the format function
     c.bench_function("format_apex", |b| {
         b.iter(|| {
             let _ = format(black_box(session.clone()));
