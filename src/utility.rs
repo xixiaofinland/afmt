1945dd3fb76724267464d5bdaf492a32f4b90faf
xi xiao <tdxiaoxi2@gmail.com>
2024-10-19 16:01:05 +0300

    +

--- src/utility.rs
+++ src/utility.rs
@@ -10,11 +10,13 @@ use log::debug;
 use tree_sitter::Node;
 
 pub fn enrich_root(con: &FmtContext) {
+    let root = &con.ast_tree.root_node();
+
     let mut global_comments = Vec::new();
     collect_comments(&root, &mut global_comments);
 
-    let top_node = &con.ast_tree.root_node().first_c();
-    let class_node = ClassNode::new(&top_node);
+    let top_node = root.first_c();
+    let classN = ClassNode::new(&top_node);
 
     let shape = EShape::empty();
     let context = EContext::new(con.config, &con.source_code);
