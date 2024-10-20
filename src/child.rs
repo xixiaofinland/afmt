44a9d190caa6f0b6ae52dae0565529a3ce612e8a
xi xiao <tdxiaoxi2@gmail.com>
2024-10-20 19:43:14 +0300

    +

--- src/child.rs
+++ src/child.rs
@@ -8,59 +8,59 @@ use tree_sitter::{Node, TreeCursor};
 // `by_n` => by name
 // `by_k` => by kind
 #[allow(dead_code)]
-pub trait Accessor<'tree> {
+pub trait Accessor<'t> {
     fn v<'a>(&self, source_code: &'a str) -> &'a str;
-    fn children_vec(&self) -> Vec<Node<'tree>>;
-    fn all_children_vec(&self) -> Vec<Node<'tree>>;
+    fn children_vec(&self) -> Vec<Node<'t>>;
+    fn all_children_vec(&self) -> Vec<Node<'t>>;
 
-    fn try_c_by_n(&self, kind: &str) -> Option<Node<'tree>>;
-    fn try_c_by_k(&self, kind: &str) -> Option<Node<'tree>>;
+    fn try_c_by_n(&self, kind: &str) -> Option<Node<'t>>;
+    fn try_c_by_k(&self, kind: &str) -> Option<Node<'t>>;
     fn try_cv_by_n<'a>(&self, name: &str, source_code: &'a str) -> Option<&'a str>;
     fn try_cv_by_k<'a>(&self, kind: &str, source_code: &'a str) -> Option<&'a str>;
-    fn try_cs_by_k(&self, kind: &str) -> Vec<Node<'tree>>;
+    fn try_cs_by_k(&self, kind: &str) -> Vec<Node<'t>>;
     fn try_csv_by_k<'a>(&self, kind: &str, source_code: &'a str) -> Vec<&'a str>;
 
-    fn c_by_n(&self, name: &str) -> Node<'tree>;
-    fn c_by_k(&self, kind: &str) -> Node<'tree>;
-    fn first_c(&self) -> Node<'tree>;
+    fn c_by_n(&self, name: &str) -> Node<'t>;
+    fn c_by_k(&self, kind: &str) -> Node<'t>;
+    fn first_c(&self) -> Node<'t>;
     fn cv_by_k<'a>(&self, name: &str, source_code: &'a str) -> &'a str;
     fn cv_by_n<'a>(&self, name: &str, source_code: &'a str) -> &'a str;
-    fn cs_by_k(&self, kind: &str) -> Vec<Node<'tree>>;
-    fn cs_by_n(&self, name: &str) -> Vec<Node<'tree>>;
+    fn cs_by_k(&self, kind: &str) -> Vec<Node<'t>>;
+    fn cs_by_n(&self, name: &str) -> Vec<Node<'t>>;
 
     fn is_comment<'a>(&self) -> bool;
 }
 
-impl<'tree> Accessor<'tree> for Node<'tree> {
+impl<'t> Accessor<'t> for Node<'t> {
     fn v<'a>(&self, source_code: &'a str) -> &'a str {
         self.utf8_text(source_code.as_bytes())
             .unwrap_or_else(|_| panic!("{}: get_value failed.", self.kind().red()))
     }
 
-    fn children_vec(&self) -> Vec<Node<'tree>> {
+    fn children_vec(&self) -> Vec<Node<'t>> {
         let mut cursor = self.walk();
         self.named_children(&mut cursor).collect()
     }
 
-    fn all_children_vec(&self) -> Vec<Node<'tree>> {
+    fn all_children_vec(&self) -> Vec<Node<'t>> {
         let mut cursor = self.walk();
         self.children(&mut cursor).collect()
     }
 
-    fn try_c_by_k(&self, kind: &str) -> Option<Node<'tree>> {
+    fn try_c_by_k(&self, kind: &str) -> Option<Node<'t>> {
         let mut cursor = self.walk();
         let child = self.named_children(&mut cursor).find(|c| c.kind() == kind);
         child
     }
 
-    fn try_cs_by_k(&self, kind: &str) -> Vec<Node<'tree>> {
+    fn try_cs_by_k(&self, kind: &str) -> Vec<Node<'t>> {
         let mut cursor = self.walk();
         self.named_children(&mut cursor)
             .filter(|c| c.kind() == kind)
             .collect()
     }
 
-    fn try_c_by_n(&self, name: &str) -> Option<Node<'tree>> {
+    fn try_c_by_n(&self, name: &str) -> Option<Node<'t>> {
         self.child_by_field_name(name)
     }
 
@@ -68,7 +68,7 @@ impl<'tree> Accessor<'tree> for Node<'tree> {
         self.child_by_field_name(name).map(|n| n.v(source_code))
     }
 
-    fn c_by_k(&self, kind: &str) -> Node<'tree> {
+    fn c_by_k(&self, kind: &str) -> Node<'t> {
         self.try_c_by_k(kind).unwrap_or_else(|| {
             panic!(
                 "{}: missing mandatory kind child: {}.",
@@ -78,7 +78,7 @@ impl<'tree> Accessor<'tree> for Node<'tree> {
         })
     }
 
-    fn first_c(&self) -> Node<'tree> {
+    fn first_c(&self) -> Node<'t> {
         self.named_child(0)
             .unwrap_or_else(|| panic!("{}: missing a mandatory child.", self.kind().red()))
     }
@@ -99,7 +99,7 @@ impl<'tree> Accessor<'tree> for Node<'tree> {
         node.v(source_code)
     }
 
-    fn c_by_n(&self, name: &str) -> Node<'tree> {
+    fn c_by_n(&self, name: &str) -> Node<'t> {
         self.child_by_field_name(name).unwrap_or_else(|| {
             panic!(
                 "{}: missing mandatory name child: {}.",
@@ -109,9 +109,9 @@ impl<'tree> Accessor<'tree> for Node<'tree> {
         })
     }
 
-    fn cs_by_n(&self, name: &str) -> Vec<Node<'tree>> {
+    fn cs_by_n(&self, name: &str) -> Vec<Node<'t>> {
         let mut cursor = self.walk();
-        let children: Vec<Node<'tree>> = self.children_by_field_name(name, &mut cursor).collect();
+        let children: Vec<Node<'t>> = self.children_by_field_name(name, &mut cursor).collect();
         if children.is_empty() {
             panic!(
                 "{}: missing mandatory name child: {}.",
@@ -122,7 +122,7 @@ impl<'tree> Accessor<'tree> for Node<'tree> {
         children
     }
 
-    fn cs_by_k(&self, kind: &str) -> Vec<Node<'tree>> {
+    fn cs_by_k(&self, kind: &str) -> Vec<Node<'t>> {
         let children = self.try_cs_by_k(kind);
         if children.is_empty() {
             panic!(
@@ -150,13 +150,13 @@ impl<'tree> Accessor<'tree> for Node<'tree> {
     }
 }
 
-pub struct NamedChildren<'tree> {
-    cursor: TreeCursor<'tree>,
-    node: Node<'tree>,
+pub struct NamedChildren<'t> {
+    cursor: TreeCursor<'t>,
+    node: Node<'t>,
 }
 
-impl<'tree> Iterator for NamedChildren<'tree> {
-    type Item = Node<'tree>;
+impl<'t> Iterator for NamedChildren<'t> {
+    type Item = Node<'t>;
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.cursor.goto_first_child() {
