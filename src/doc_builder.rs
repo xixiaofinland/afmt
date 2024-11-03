f007a39222c0e44c5f5a44b4e41f56d98af3695c
xi xiao <tdxiaoxi2@gmail.com>
2024-11-03 13:34:45 +0200

    +

--- src/doc_builder.rs
+++ src/doc_builder.rs
@@ -9,6 +9,63 @@ impl<'a> DocBuilder<'a> {
         DocBuilder(Arena::new())
     }
 
+    pub fn group(&'a self, doc_ref: DocRef<'a>) -> DocRef<'a> {
+        let flat_n = self.flat(doc_ref);
+        self.choice(flat_n, doc_ref)
+    }
+
+    pub fn softline(&'a self) -> DocRef<'a> {
+        let space = self.txt(" ");
+        let newline = self.nl();
+        self.choice(space, newline)
+    }
+
+    pub fn maybeline(&'a self) -> DocRef<'a> {
+        let empty = self.txt("");
+        let newline = self.nl();
+        self.choice(empty, newline)
+    }
+
+    fn comma_sep_single_line(&'a self, elems: &[DocRef<'a>]) -> DocRef<'a> {
+        let mut list = self.flat(elems[0]);
+        for elem in &elems[1..] {
+            list = self.concat([list, self.txt(", "), self.flat(elem)]);
+        }
+        list
+    }
+
+    fn comma_sep_multi_line(&'a self, elems: &[DocRef<'a>]) -> DocRef<'a> {
+        let mut list = elems[0];
+        for elem in &elems[1..] {
+            list = self.concat([list, self.txt(", "), self.nl(), elem]);
+        }
+        list
+    }
+
+    fn surrounded(&'a self, open: &str, elems: &[DocRef<'a>], closed: &str) -> DocRef<'a> {
+        if elems.is_empty() {
+            return self.txt(format!("{}{}", open, closed));
+        }
+
+        let single_line = self.concat([
+            self.txt(open),
+            self.comma_sep_single_line(elems),
+            self.txt(closed),
+        ]);
+        let multi_line = self.concat([
+            self.txt(open),
+            self.indent(
+                4,
+                self.concat([self.nl(), self.comma_sep_multi_line(elems)]),
+            ),
+            self.nl(),
+            self.txt(closed),
+        ]);
+        self.choice(single_line, multi_line)
+    }
+
+    // fundamental blocks
+
     pub fn nl(&'a self) -> DocRef<'a> {
         self.0.alloc(Doc::Newline)
     }
@@ -34,22 +91,5 @@ impl<'a> DocBuilder<'a> {
 
     pub fn choice(&'a self, first: DocRef<'a>, second: DocRef<'a>) -> DocRef<'a> {
         self.0.alloc(Doc::Choice(first, second))
-    }
-
-    pub fn group(&'a self, doc_ref: DocRef<'a>) -> DocRef<'a> {
-        let flat_n = self.flat(doc_ref);
-        self.choice(flat_n, doc_ref)
-    }
-
-    pub fn softline(&'a self) -> DocRef<'a> {
-        let space = self.txt(" ");
-        let newline = self.nl();
-        self.choice(space, newline)
-    }
-
-    pub fn maybeline(&'a self) -> DocRef<'a> {
-        let empty = self.txt("");
-        let newline = self.nl();
-        self.choice(empty, newline)
     }
 }
