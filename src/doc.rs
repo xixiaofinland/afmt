20a169df4a0e51192eae071658af6952f5b3eb0a
xi xiao <tdxiaoxi2@gmail.com>
2024-11-08 00:26:26 +0200

    +

--- src/doc.rs
+++ src/doc.rs
@@ -96,12 +96,12 @@ impl<'a> PrettyPrinter<'a> {
                     self.col = chunk.indent;
                 }
                 Doc::Text(text, width) => {
-                    if self.col == 0 {
-                        for _ in 0..chunk.indent {
-                            result.push(' ');
-                        }
-                        self.col += chunk.indent;
-                    }
+                    //if self.col == 0 {
+                    //    for _ in 0..chunk.indent {
+                    //        result.push(' ');
+                    //    }
+                    //    self.col += chunk.indent;
+                    //}
                     result.push_str(text);
                     self.col += width;
                 }
