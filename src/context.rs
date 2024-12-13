d12c0c2152543302c52c3183f897190355b75d96
xi xiao <tdxiaoxi2@gmail.com>
2024-12-13 21:35:35 +0200

    +

--- src/context.rs
+++
@@ -1,6 +0,0 @@
-use crate::accessor::Accessor;
-use crate::rich_def::*;
-use anyhow::Result;
-use colored::Colorize;
-use tree_sitter::{Language, Node, Parser, Tree};
-
--- src/lib.rs
+++ src/lib.rs
@@ -5,16 +5,7 @@ mod doc;
 mod doc_builder;
 mod enum_def;
 pub mod formatter;
-mod macros;
 mod utility;
-//mod shape;
-//mod rewrite;
-//mod rich_macro;
-//mod rich_node;
-//mod rich_structs;
-//mod route;
-//mod fmt_push;
-//mod visit;
 
 use formatter::Formatter;
 
