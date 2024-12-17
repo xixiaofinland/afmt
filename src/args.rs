2e697b1abf0f9e2f5fae10ae898a3c4f41c81671
xi xiao <tdxiaoxi2@gmail.com>
2024-12-17 09:02:41 +0200

    +

--- src/args.rs
+++ src/args.rs
@@ -8,7 +8,7 @@ pub struct Args {
 }
 
 pub fn get_args() -> Args {
-    const VERSION: &str = "v0.0.25";
+    const VERSION: &str = "v0.0.26";
 
     let matches = Command::new("afmt")
         .version(VERSION)
