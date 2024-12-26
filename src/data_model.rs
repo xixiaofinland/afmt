73972884e7d87aa33011e09a7f81f4792f19ccdd
xi xiao <tdxiaoxi2@gmail.com>
2024-12-26 18:43:20 +0200

    +

--- src/data_model.rs
+++ src/data_model.rs
@@ -2825,32 +2825,25 @@ impl InterfaceDeclaration {
 
 impl<'a> DocBuild<'a> for InterfaceDeclaration {
     fn build_inner(&self, b: &'a DocBuilder<'a>, result: &mut Vec<DocRef<'a>>) {
-        let bucket = get_comment_bucket(&self.node_info.id);
-        if !bucket.dangling_comments.is_empty() {
-            return result.push(b.concat(handle_dangling_comments(b, bucket)));
-        }
+        build_with_comments(b, &self.node_info.id, result, |b, result| {
+            if let Some(ref n) = self.modifiers {
+                result.push(n.build(b));
+            }
 
-        handle_pre_comments(b, bucket, result);
+            result.push(b.txt_("interface"));
+            result.push(self.name.build(b));
 
-        if let Some(ref n) = self.modifiers {
-            result.push(n.build(b));
-        }
+            if let Some(ref n) = self.type_parameters {
+                result.push(n.build(b));
+            }
 
-        result.push(b.txt_("interface"));
-        result.push(self.name.build(b));
+            if let Some(ref n) = self.extends {
+                result.push(n.build(b));
+            }
 
-        if let Some(ref n) = self.type_parameters {
-            result.push(n.build(b));
-        }
-
-        if let Some(ref n) = self.extends {
-            result.push(n.build(b));
-        }
-
-        result.push(b.txt(" "));
-        result.push(self.body.build(b));
-
-        handle_post_comments(b, bucket, result);
+            result.push(b.txt(" "));
+            result.push(self.body.build(b));
+        });
     }
 }
 
