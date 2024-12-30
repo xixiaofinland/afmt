1e003a1f6affda99ff1ed0cbf9553fbf4a92bd7f
xi xiao <tdxiaoxi2@gmail.com>
2024-12-30 21:25:40 +0200

    +

--- src/utility.rs
+++ src/utility.rs
@@ -234,7 +234,9 @@ pub fn build_with_comments<'a, F>(
     handle_pre_comments(b, bucket, result);
 
     if bucket.dangling_comments.is_empty() {
-        handle_members(b, result);
+        let mut members_docs = Vec::new();
+        handle_members(b, &mut members_docs);
+        result.push(b.concat(members_docs));
     } else {
         result.push(b.concat(handle_dangling_comments(b, bucket)));
         return;
