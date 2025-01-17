f4d317bb02f7915e5def9853df1fda1d3127721d
xi xiao <tdxiaoxi2@gmail.com>
2025-01-17 09:56:14 +0200

    feat: show the version info in afmt -h

--- src/args.rs
+++ src/args.rs
@@ -8,7 +8,7 @@ pub struct Args {
 }
 
 pub fn get_args() -> Args {
-    const VERSION: &str = "v0.1.1";
+    const VERSION: &str = "v0.2.0";
 
     let matches = Command::new("afmt")
         .version(VERSION)
